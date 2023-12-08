226a7d881c7e4b6395f657a10312de325ffbf4f5
klanti117 <klantyislam@yahoo.com>
2023-12-08 11:07:23 -0500

    server running port 3100

--- src/App.js
+++ src/App.js
@@ -1,36 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import AddPet from "./AddPet";
 import ListPets from "./ListPets";
 import "./index.css";
 
 function App() {
-  const [pets, setPets] = useState([
-    {
-      id: 1,
-      petName: "Fluffy",
-      profilePicture:
-        "https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?q=80&w=2886&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      species: "dog",
-      isFriendly: false,
-    },
-    {
-      id: 2,
-      petName: "Rex",
-      profilePicture:
-        " https://images.unsplash.com/photo-1537151625747-768eb6cf92b2?q=80&w=2568&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      species: "cat",
-      isFriendly: true,
-    },
-    {
-      id: 3,
-      petName: "Screaming Jean",
-      profilePicture:
-        "https://images.unsplash.com/photo-1608374398721-38b323502c5c?q=80&w=2118&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      species: "bird",
-      isFriendly: false,
-    },
-  ]);
+  const [pets, setPets] = useState([]);
+
+  useEffect(() => {
+    const fetchPets = async () => {
+      const res = await fetch("http://localhost:3100/pets");
+      const data = await res.json();
+
+      console.log(data);
+    };
+    fetchPets();
+  }, []);
 
   //Add pets
   const addPet = (pet) => {
